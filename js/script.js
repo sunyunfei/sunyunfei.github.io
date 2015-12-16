643d7e19f09a17ab806db145e352d6bf5f7a00b5
孙云飞 <sunyunfei@yangliwu.com>
2015-12-16 09:31:49 +0800

    Site updated: 2015-12-16 09:31:49

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
 (function($){
+  /*toTop start*/
+  // When to show the scroll link
+  // higher number = scroll link appears further down the page
+  var upperLimit = 1000;
+  // Our scroll link element
+  var scrollElem = $('#totop');
+  // Scroll to top speed
+  var scrollSpeed = 500;
+  // Show and hide the scroll to top link based on scroll position
+  $(window).scroll(function() {
+    var scrollTop = $(document).scrollTop();
+    if (scrollTop > upperLimit) {
+      $(scrollElem).stop().fadeTo(300, 1); // fade back in
+    } else {
+      $(scrollElem).stop().fadeTo(300, 0); // fade out
+    }
+  });
+
+  // Scroll to top animation on click
+  $(scrollElem).click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, scrollSpeed);
+    return false;
+  });
+  /*toTop end*/
   // Search
   var $searchWrap = $('#search-form-wrap'),
     isSearchAnim = false,
@@ -38,42 +64,62 @@
     e.stopPropagation();
 
     var $this = $(this),
-      url = $this.attr('data-url'),
-      encodedUrl = encodeURIComponent(url),
-      id = 'article-share-box-' + $this.attr('data-id'),
+      type = $this.attr('data-share'),
       offset = $this.offset();
 
-    if ($('#' + id).length){
-      var box = $('#' + id);
+    if (type == 'baidu') {
+      var box = $('#article-share-box');
+      shareDataUrl = $this.attr('data-url');
+      shareDataTitle = $this.attr('data-title');
 
       if (box.hasClass('on')){
         box.removeClass('on');
         return;
       }
-    } else {
-      var html = [
-        '<div id="' + id + '" class="article-share-box">',
-          '<input class="article-share-input" value="' + url + '">',
-          '<div class="article-share-links">',
-            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
-            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-            '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
-            '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
-          '</div>',
-        '</div>'
-      ].join('');
 
-      var box = $(html);
+      $('.article-share-box.on').hide();
 
-      $('body').append(box);
-    }
+      box.css({
+        top: offset.top + 25,
+        left: offset.left - 25
+      }).addClass('on');
+    } else{
+      var url = $this.attr('data-url'),
+      encodedUrl = encodeURIComponent(url),
+      id = 'article-share-box-' + $this.attr('data-id');
 
-    $('.article-share-box.on').hide();
+      if ($('#' + id).length){
+        var box = $('#' + id);
 
-    box.css({
-      top: offset.top + 25,
-      left: offset.left
-    }).addClass('on');
+        if (box.hasClass('on')){
+          box.removeClass('on');
+          return;
+        }
+      } else {
+        var html = [
+          '<div id="' + id + '" class="article-share-box">',
+            '<input class="article-share-input" value="' + url + '">',
+            '<div class="article-share-links">',
+              '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
+              '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
+              '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
+              '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
+            '</div>',
+          '</div>'
+        ].join('');
+
+        var box = $(html);
+
+        $('body').append(box);
+      }
+
+      $('.article-share-box.on').hide();
+
+      box.css({
+        top: offset.top + 25,
+        left: offset.left
+      }).addClass('on');
+    };
   }).on('click', '.article-share-box', function(e){
     e.stopPropagation();
   }).on('click', '.article-share-box-input', function(){
